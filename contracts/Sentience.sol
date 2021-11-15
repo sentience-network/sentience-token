ef9722302144663f7cd39dadbf5ea5c8e3ad275c
Kingsley Victor <javaprodigy56@gmail.com>
2021-11-15 13:38:46 +0100

    Add comments

--- contracts/Sentience.sol
+++ contracts/Sentience.sol
@@ -22,22 +22,41 @@ contract Sentience is Context, Ownable, ERC20 {
     _minters[_msgSender()] = true;
   }
 
+  /** @dev Adds a minter. Can only be executed by contract owner
+   *  @param minter_ The address to add as minter
+   */
   function addMinter(address minter_) external onlyOwner {
     _minters[minter_] = true;
   }
 
+  /** @dev Removes a minter. Can only be executed by contract owner
+   *  @param minter_ The address to remove as a minter
+   */
   function removeMinter(address minter_) external onlyOwner {
     _minters[minter_] = false;
   }
 
+  /** @dev Mints tokens. Can only be called by a minter
+   *  @param account_ Address to mint tokens for
+   *  @param amount_ Amount of tokens to mint
+   */
   function mint(address account_, uint256 amount_) external onlyMinter {
     _mint(account_, amount_);
   }
 
+  /** @dev Burns token. Can only be called by a minter
+   *  @param account_ Address whose tokens should be burned
+   *  @param amount_ Amount of tokens to burn
+   */
   function burn(address account_, uint256 amount_) external onlyMinter {
     _burn(account_, amount_);
   }
 
+  /** @dev Retrieve ERC20/BEP20 tokens sent to contract. Can only be called by contract owner
+   *  @param token_ Address of token to retrieve
+   *  @param to_ Address to send tokens to
+   *  @param amount_ Amount of tokens to send
+   */
   function retrieveToken(
     address token_,
     address to_,
@@ -61,7 +80,11 @@ contract Sentience is Context, Ownable, ERC20 {
     );
   }
 
-  function retrieveBNB(address payable to_, uint256 amount_)
+  /** @dev Retrieve Ether sent to contract. Can only be called by contract owner
+   *  @param to_ Address to send Ether to
+   *  @param amount_ Amount of Ether to send
+   */
+  function retrieveEther(address payable to_, uint256 amount_)
     external
     onlyOwner
   {
